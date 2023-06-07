fd1ca89ee15a5844ab3b8168be8b3af914bb43b5
antoniomsah <amsah@usp.br>
2023-06-07 15:40:11 -0300

    Added basic template

---
+++ handbook/extra/template.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+
+#define fastio ios_base::sync_with_stdio(0); cin.tie(0)
+#define int long long
+
+using namespace std;
+
+void dbg_out() { cerr << endl; }
+template <typename H, typename... T>
+void dbg_out(H h, T... t) { cerr << ' ' << h; dbg_out(t...); }
+#define dbg(...) { cerr << #__VA_ARGS__ << ':'; dbg_out(__VA_ARGS__); }
+
+signed main(){
+	fastio;
+	return 0;
+}
