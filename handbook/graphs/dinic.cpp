5e48b853d84aa56587dd8bd0f085b220befb3d9d
antoniomsah <amsah@usp.br>
2023-06-12 10:53:52 -0300

    Dinitz's algorithm improved.

--- handbook/graphs/dinic.cpp
+++ handbook/graphs/dinic.cpp
@@ -1,10 +1,3 @@
-/**
- * Dinitz's algorithm for finding maximum flows.
- *
- * Complexity: O(n^2m) (O(m sqrt(n)) on unit networks)
- **/
-
-
 template <typename T>
 struct Dinic {
 	struct Edge {
@@ -14,18 +7,20 @@ struct Dinic {
 	};
 
 	int s,t;
-	vector<T> dist, ptr, q;
-	vector<vector<int>> adj;
+	vector<int> nxt, ptr, frst, dist, q;
 	vector<Edge> edges;
 
 	Dinic(int _s, int _t, int n) : 
-		s(_s), t(_t), dist(n), ptr(n), adj(n), q(n) {}
+		s(_s), t(_t), dist(n), frst(n,-1), q(n) {}
 
-	void add_edge(int u, int v, int cap){
-		edges.push_back({v,cap});
-		adj[u].push_back(edges.size()-1);
-		edges.push_back({u,0});
-		adj[v].push_back(edges.size()-1);
+	void add(int u, int v, int cap){
+		edges.emplace_back(v,cap);
+		nxt.push_back(frst[u]);
+		frst[u] = edges.size()-1;
+
+		edges.emplace_back(u,0);
+		nxt.push_back(frst[v]);
+		frst[v] = edges.size()-1;
 	}
 
 	bool bfs() {
@@ -35,9 +30,9 @@ struct Dinic {
 		dist[s] = 0;
 		while(st<ed){
 			int u = q[st++];
-			for(int i=0; i<adj[u].size(); i++){
-				int idx = adj[u][i], v = edges[idx].to;
-				if(dist[v] == -1 and edges[idx].cap > edges[idx].flow){
+			for(int e=frst[u]; e != -1; e = nxt[e]) {
+				int v = edges[e].to;
+				if(dist[v] == -1 and edges[e].cap > edges[e].flow){
 					dist[v] = dist[u]+1;
 					q[ed++] = v;
 				}
@@ -48,13 +43,13 @@ struct Dinic {
 
 	T dfs(int u, T f=INF){
 		if(u == t or f == 0) return f;
-		for(int &i=ptr[u]; i<adj[u].size(); i++){
-			int idx = adj[u][i], v = edges[idx].to;
-			if(dist[u]+1 == dist[v] and edges[idx].cap > edges[idx].flow){
-				int df = dfs(v, min(f,edges[idx].cap-edges[idx].flow));
+		for(int &e=ptr[u]; e != -1; e=nxt[e]) {
+			int v = edges[e].to;
+			if(dist[u]+1 == dist[v] and edges[e].cap > edges[e].flow){
+				int df = dfs(v, min(f,edges[e].cap-edges[e].flow));
 				if(df>0){
-					edges[idx].flow += df;
-					edges[idx^1].flow -= df;
+					edges[e].flow += df;
+					edges[e^1].flow -= df;
 					return df;
 				}
 			}
@@ -65,7 +60,7 @@ struct Dinic {
 	T flow() {
 		T flow=0;
 		while(bfs()){
-			fill(ptr.begin(), ptr.end(), 0);
+			ptr=frst;
 			while(T add_flow=dfs(s)) flow += add_flow;
 		}
 		return flow;
