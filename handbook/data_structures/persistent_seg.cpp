b051ef782894c904b0719c8a37e2beccb06f54c1
Matheus Paulo <53917417+matheuzinn@users.noreply.github.com>
2023-06-17 00:02:29 -0300

    Update persistent_seg.cpp

--- handbook/data_structures/persistent_seg.cpp
+++ handbook/data_structures/persistent_seg.cpp
@@ -4,32 +4,32 @@ template <typename T>
 struct pseg{
 	const int LIM = 1e7;
     int n, rt, nxt;
-    vector<T> st; 
+    vector<T> seg; 
 	vector<int> L, R;
-	pseg(int n): st(LIM),L(LIM),R(LIM),n(n),nxt(0){}
+	pseg(int n): seg(LIM),L(LIM),R(LIM),n(n),nxt(0){}
 	int new_node(T x, int l=0, int r=0){
 		int nn = nxt++;
-		st[nn] = x; L[nn] = l; R[nn] = r;
+		seg[nn] = x; L[nn] = l; R[nn] = r;
 		return nn;
 	}
 	int build(vector<T> &v, int l, int r){
 		if(l==r) return new_node(v[l]);
 		int m=(l+r)/2, nl=build(v,l,m), nr=build(v,m+1,r);
-		return new_node(st[nl]+st[nr],nl,nr);
+		return new_node(seg[nl]+seg[nr],nl,nr);
 	}
 	int build(vector<T> &v){return rt = build(v,0,n-1);}
 	int update(int p, T x, int k, int l, int r){
-		int nn = new_node(st[k],L[k],R[k]);
-		if(l == r){st[nn]=x;return nn;}
+		int nn = new_node(seg[k],L[k],R[k]);
+		if(l == r){seg[nn]=x;return nn;}
 		int m = (l+r)/2;
 		if(p <= m) L[nn] = update(p,x,L[nn],l,m);
 		else R[nn] = update(p,x,R[nn],m+1,r);
-		st[nn] = st[L[nn]] + st[R[nn]];
+		seg[nn] = seg[L[nn]] + seg[R[nn]];
 		return nn;
 	}
 	T query(int a, int b, int k, int l, int r){
 		if(r < a or b < l) return 0;
-		if(a <= l and r <= b) return st[k];
+		if(a <= l and r <= b) return seg[k];
 		int m = (l+r)/2;
 		return query(a,b,L[k],l,m) + query(a,b,R[k],m+1,r);
 	}
