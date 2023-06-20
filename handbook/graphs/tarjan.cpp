ac55fc728903894e5d9e91519b558381f1c19cf6
antoniomsah <amsah@usp.br>
2023-06-19 21:19:28 -0300

    Refactored tarjan algorithm for strongly connected components

--- handbook/graphs/tarjan.cpp
+++ handbook/graphs/tarjan.cpp
@@ -1,21 +1,55 @@
-// Tarjan
+/**
+ * Tarjan's algorithm for strongly connected components
+ *
+ * Description: 
+ * 		Builds an array scc such that scc[u] = scc[v] 
+ * 		iff u and v are in the same component. And build an
+ * 		array sz such that sz[scc[u]] is the size of u's component.
+ *
+ * Complexity: O(n+m)
+ **/
 
-    stack<int> s;
-    vector<int> vis(n), comp(n), id(n);
-    int t = 0;
-    function<int(int)> dfs = [&](int x){
-        int lo = id[x] = t++;
-        s.push(x);
-        vis[x] = 2;
-        for(int v:adj[x]){
-            if(!vis[v]) lo = min(lo, dfs(v));
-            else if(vis[v] == 2) lo = min(lo, id[v]);
-        }
-        if(lo == id[x]) while(true){
-            int v = s.top(); s.pop();
-            vis[v] = 1, comp[v] = x;
-            if (v == x) break;
-        }
-        return lo;
-    };
-    for(int i=0;i<n;i++) if(!vis[i]) dfs(i);
+struct tarjan_scc {
+	int nscc;
+	vector<int> scc, sz;
+
+	tarjan_scc(int n, vector<int> adj[]) : nscc(0), scc(n) {
+		int t=0, sn=0;
+		vector<int> disc(n,-1), low(n,-1), stk(n);
+
+		function<void(int)> dfs = [&](int u) {
+			disc[u] = low[u] = t++;
+			stk[sn++] = u;
+			for(auto v: adj[u]) {
+				if(disc[v] == -1) dfs(v);
+				low[u] = min(low[u], low[v]);
+			}
+			if(low[u] == disc[u]) {
+				int v;
+				sz.emplace_back(0);
+				do {
+					v = stk[--sn];
+					scc[v] = nscc;
+					sz[nscc]++;
+					low[v] = INF;
+				} while(u != v);
+				nscc++;
+			}
+		};
+
+		for(int i=0; i<n; i++) {
+			if(disc[i] == -1) dfs(i);
+		}
+	}
+
+	// Complexity: O(n+m)
+	vector<vector<int>> condensed_graph(int n, vector<int> adj[]) {
+		vector<vector<int>> adj_scc(nscc);
+		for(int u=0; u<n; u++) {
+			for(int v: adj[u]) {
+				if(scc[v] != scc[u]) adj_scc[scc[u]].push_back(scc[v]);
+			}
+		}
+		return adj_scc;
+	}
+};
