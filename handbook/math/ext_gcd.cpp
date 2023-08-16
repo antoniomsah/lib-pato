d065bb08f71ec2d2a8ec686f43685565cd84b9aa
Matheus Paulo <53917417+matheuzinn@users.noreply.github.com>
2023-08-16 14:07:17 -0300

    Update ext_gcd.cpp

--- handbook/math/ext_gcd.cpp
+++ handbook/math/ext_gcd.cpp
@@ -3,7 +3,7 @@
 // Let g = gcd(a,b)
 // Returns {g,x,y} such that ax+by = g
 
-array<int,3> ext_gcd(int a, int b) {
+tuple<int,int,int> ext_gcd(int a, int b) {
     if (!a) return {b, 0, 1};
     auto [g, x, y] = ext_gcd(b%a, a);
     return {g, y - b/a*x, x};
