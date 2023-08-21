a64470f6115fae50ac9cfba5692bec4134f707d4
Matheus Paulo <53917417+matheuzinn@users.noreply.github.com>
2023-08-21 16:33:37 -0300

    Create crt.cpp

---
+++ handbook/math/crt.cpp
@@ -0,0 +1,24 @@
+// Chinese Remainder Theorem
+// Joins modular linear equations: x = a (mod m)
+// if there are no solutions, a = -1
+
+template<typename T> tuple<T, T, T> ext_gcd(T a, T b) {
+    if (!a) return {b, 0, 1};
+    auto [g, x, y] = ext_gcd(b%a, a);
+    return {g, y - b/a*x, x};
+}
+
+template<typename T = ll> struct crt {
+	T a, m;
+
+	crt() : a(0), m(1) {}
+	crt(T a_, T m_) : a(a_), m(m_) {}
+	crt operator * (crt C) {
+		auto [g, x, y] = ext_gcd(m, C.m);
+		if ((a - C.a) % g) a = -1;
+		if (a == -1 or C.a == -1) return crt(-1, 0);
+		T lcm = m/g*C.m;
+		T ans = a + (x*(C.a-a)/g % (C.m/g))*m;
+		return crt((ans % lcm + lcm) % lcm, lcm);
+	}
+};
