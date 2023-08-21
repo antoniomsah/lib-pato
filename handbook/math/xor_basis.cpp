bc263b4a48a5ba2e174b2b79cfdd410cbd6e1ead
Matheus Paulo <53917417+matheuzinn@users.noreply.github.com>
2023-08-21 16:47:54 -0300

    Update xor_basis.cpp

--- handbook/math/xor_basis.cpp
+++ handbook/math/xor_basis.cpp
@@ -1,8 +1,8 @@
-// xor basis
+// Xor basis
 
 const int d = 30;
 vector<int> basis(d);
-auto insertVector = [&](int x){
+auto add = [&](int x){
 	for(int i=d-1;i>=0;i--) {
 		if((x&(1<<i)) == 0) continue;
 		if(!basis[i]){
