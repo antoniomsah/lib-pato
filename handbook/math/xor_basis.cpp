159ca1cc70ac17631f0a4fc88c6707f2eb73aced
antoniomsah <amsah@usp.br>
2023-12-03 14:50:40 -0300

    Refactor XOR basis structure

--- handbook/math/xor_basis.cpp
+++ handbook/math/xor_basis.cpp
@@ -1,14 +1,61 @@
-// Xor basis
+// XOR basis
+//
+// Description: 
+// 		builds a basis in \Integers_2 that spans the
+// 		vectors inserted.
+//
+// 		think of integers as vectors in \Integers_2,
+// 		i.e., 4 -> (1,0,0).
+//
+// Complexity: O(SZ), unless stated otherwise
 
-const int d = 30;
-vector<int> basis(d);
-auto add = [&](int x){
-	for(int i=d-1;i>=0;i--) {
-		if((x&(1<<i)) == 0) continue;
-		if(!basis[i]){
-			basis[i] = x;
-			return;
+
+template <const int SZ>
+struct XorBasis {
+	int base[SZ];
+
+	XorBasis() { memset(base, 0, sizeof base); }
+
+	bool bit_on(int x, int i) {
+		return (x&(1LL<<i));
+	}
+
+	void insert(int v) {
+		for (int i = SZ-1; i >= 0; i--) {
+			if (bit_on(v, i)) {
+				if (!base[i]) {
+					base[i] ^= v; 
+					return;
+				}
+				v ^= base[i];
+			}
 		}
-		x ^= basis[i];
+	}
+
+	// merges two basis in O(SZ^2)
+	void merge(XorBasis &b) {
+		for (int i = SZ-1; i >= 0; i--) {
+			if (b.base[i]) insert(b.base[i]);
+		}
+	}
+
+	// checks if v can be achieved
+	bool check(int v) {
+		int x = 0;
+		for (int i = SZ-1; i >= 0; i--) if (base[i]) {
+			if (bit_on(x, i) != bit_on(v, i)) {
+				x ^= base[i];
+			}
+		}
+		return (x == v);
+	}
+
+	// returns the maximum value
+	int max() {
+		int value = 0;
+		for (int i = SZ-1; i >= 0; i--) if (!bit_on(value, i)) {
+			value ^= base[i];
+		}
+		return value;
 	}
 };
