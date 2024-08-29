b80c3fe26cb5c93104fee0e80b444ea816f55ba1
Matheus Paulo <53917417+matheuzinn@users.noreply.github.com>
2024-08-29 13:16:04 -0300

    Update bit.cpp

--- handbook/structures/bit.cpp
+++ handbook/structures/bit.cpp
@@ -2,40 +2,45 @@
 //
 // Functions:
 // 		query(l,r):	 sum of elements in range [l,r]
-// 		update(i,x): sums x to value in position i
+// 		update(i,x): 	 sums x to value in position i
+// 		upper_bound(x):  smallest p st pref(p) > x
 //
-// Details: query and update are 1-indexed.
+// Details: 0-indexed
 //
 // Complexity:
-// 		build: 	O(n)
-// 		query: 	O(lgn)
-// 		update:	O(lgn)
+// 		build: 	     O(n)
+// 		query: 	     O(lgn)
+// 		update:	     O(lgn)
+//		upper_bound: O(lgn)
 
-template <typename T>
-struct BIT{
+template<typename T>
+struct BIT {
 	int n;
 	vector<T> bit;
-
-	BIT(int n) : n(n+1), bit(n+1){}
-	BIT(vector<int> &v) : n(ssize(v)+1), bit(ssize(v)+1) {
-		for(int i=1;i<n;i++) bit[i] = v[i-1];
-		for(int i=1;i<n;i++) {
-			int j = i+(i&-i);
-			if(j<n) bit[j] += bit[i];
+	BIT(int _n=0) : n(_n), bit(n + 1) {}
+	BIT(vector<T> &v) : n(v.size()), bit(n + 1) {
+		for (int i = 1; i <= n; i++) {
+			bit[i] += v[i - 1];
+			int j = i + (i & -i);
+			if (j <= n) bit[j] += bit[i];
 		}
 	}
-
-	T pref_query(int i) {
-		T sum = 0;
-		for(;i;i-=i&-i) sum+=bit[i];
-		return sum;
+	void update(int i, T x) { // soma x na posicao i
+		for (i++; i <= n; i += i & -i) bit[i] += x;
 	}
-
-	T query(int l, int r) {
-		return pref_query(r)-pref_query(l-1);
+	T pref(int i) { // soma [0, i]
+		T ret = 0;
+		for (i++; i; i -= i & -i) ret += bit[i];
+		return ret;
 	}
-
-	void update(int i, T x) {
-		for(;i<n;i+=i&-i) bit[i]+=x;
+	T query(int l, int r) {  // soma [l, r]
+		return pref(r) - pref(l - 1); 
+	}
+	int upper_bound(T x) {
+		int p = 0;
+		for (int i = __lg(n); i+1; i--) 
+			if (p + (1<<i) <= n and bit[p + (1<<i)] <= x)
+				x -= bit[p += (1 << i)];
+		return p;
 	}
 };
